bd2ad49e40f3ad1740dc4510a4560262c376039e
Mark Talent <mtalent@live.com>
2022-05-16 17:38:15 -0400

    song adapters

--- app/src/main/java/com/assignment/marktalentitunes/SecondFragment.kt
+++ app/src/main/java/com/assignment/marktalentitunes/SecondFragment.kt
@@ -5,7 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.assignment.marktalentitunes.api.ApiService
+import com.assignment.marktalentitunes.model.ResultSongListing
+import com.assignment.marktalentitunes.view.SongAdapterSecond
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+
 class SecondFragment : Fragment() {
+
+    lateinit var rvSongList: RecyclerView
+    lateinit var songAdapter: SongAdapterSecond
+
 
 
 
@@ -14,9 +28,37 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+        rvSongList = view.findViewById(R.id.rv_song_list_blue)
+        startRetrofit()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return view //inflater.inflate(R.layout.fragment_first, container, false)
     }
+
+
+    private fun startRetrofit() {
+        ApiService.createRetrofit().create(ApiService::class.java).getSongListing()
+            .enqueue(object : Callback<ResultSongListing> {
+                override fun onResponse(
+                    call: Call<ResultSongListing>,
+                    response: Response<ResultSongListing>
+                ) {
+                    if (response.isSuccessful) {
+                        songAdapter = SongAdapterSecond(response.body()!!.results)
+                        rvSongList.adapter = songAdapter
+                    }
+                }
+
+                override fun onFailure(call: Call<ResultSongListing>, t: Throwable) {
+
+                }
+
+            })
+    }
+
+
+
 
 
 }
